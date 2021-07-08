15050201fea0963df2e69ab930481bb4ef0424bf
Nadia Ivanovskaya <nadia.ivanovskaya@softensity.com>
2021-07-08 22:54:41 +0300

    Basic react native app + Eslint

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  arrowParens: 'avoid'
+};
